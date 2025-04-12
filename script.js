dd6b2c50083ff382a45341da3e84fd0985258f92
Hansh Raj Joshi <joshihanshraj635@gmail.com>
2025-04-12 12:29:54 +0545

    update the files

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-window.addEventListener('load', () => {
-    const butterfly = document.getElementById('butterfly');
-    setTimeout(() => {
-      butterfly.remove();
-    }, 4500); // remove after animation completes
-  });
-  
+// Butterfly entrance animation
+window.addEventListener("load", () => {
+  const butterfly = document.createElement("div");
+  butterfly.innerText = "🦋";
+  butterfly.style.position = "fixed";
+  butterfly.style.left = "-50px";
+  butterfly.style.top = "100px";
+  butterfly.style.fontSize = "40px";
+  butterfly.style.zIndex = "1000";
+  butterfly.style.transition = "all 5s ease-in-out";
+  document.body.appendChild(butterfly);
+
+  setTimeout(() => {
+    butterfly.style.left = "100vw";
+    butterfly.style.top = "20px";
+    butterfly.style.opacity = "0";
+  }, 5000);
+
+  setTimeout(() => {
+    butterfly.remove();
+  }, 7000);
+});
